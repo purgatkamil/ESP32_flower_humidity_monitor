ae82c3fcb368dd0aba707aa03aa77a9fc0b580b5
Kamil Purgat <kpurgat.zsp9@gmail.com>
2025-01-19 11:39:09 +0100

    Add spiffs init to app_main

--- main/main.c
+++ main/main.c
@@ -7,11 +7,14 @@
 #include "esp_log.h"
 
 #include "wifi.h"
+#include "spiffs.h"
 
 void app_main(void)
 {
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
+    spiffs_init();
+    spiffs_list_files();
     wifi_init();
 }
